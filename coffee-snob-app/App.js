b0eb17d836a6d22bcd819b9b92f7079ab60978c9
Tim Itayi <timitayi@Timothys-Mac.local>
2024-03-19 17:43:00 +1300

    update expo config

--- coffee-snob-app/App.js
+++ coffee-snob-app/App.js
@@ -3,32 +3,43 @@ import React , {useEffect , useState } from 'react';
 import * as Font from 'expo-font';
 import { StatusBar } from 'expo-status-bar'
 import Home  from './components/home';
-
+import {Text} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { registerRootComponent } from 'expo';
 
 const Stack = createStackNavigator();
 
 
 
-
+registerRootComponent(App);
 export default function App() {
 
   const [fontsLoaded, setFontsLoaded] = useState(false);
+  const [errorLoadingFonts, setErrorLoadingFonts] = useState(false);
 
   useEffect(() => {
     async function loadFonts() {
+     try {
       await Font.loadAsync({
         'MillikRegular': require('./assets/fonts/Millik.otf'),
       });
       setFontsLoaded(true);
-    }
+     } catch (error) {
+
+      console.error('Error loading fonts', error);
+      setErrorLoadingFonts(true);
+     }
+      
+    } 
 
     loadFonts();
   }, []);
 
   if (!fontsLoaded) {
+    if( errorLoadingFonts){
+      return <Text>Error loading fonts. Please try again later</Text>;
+    }
 
     return null;
   }
--- coffee-snob-app/components/home.js
+++ coffee-snob-app/components/home.js
@@ -1,5 +1,6 @@
 import React, { useRef , useState} from 'react';
-import { Animated,Easing , Dimensions } from "react-native";
+import { Easing , Dimensions } from "react-native";
+import {Animated as Reanimated }from 'react-native-reanimated';
 import CafeList from './cafeList';
 import styles from '../AppStyles'
 import HeaderTitle from './HeaderTitle';
@@ -7,51 +8,114 @@ import HeaderTitle from './HeaderTitle';
 const screenHeight = Dimensions.get('window').height;
 
 export default function Home() {
-    const scrollY = useRef(new Animated.Value(0)).current;
-    const diffClamp = Animated.diffClamp(scrollY, 0 , 100);
+    const scrollY = useRef(new Reanimated.Value(0)).current;
+    const diffClamp = Reanimated.diffClamp(scrollY, 0 , 100);
     const [prevScrollY , setPrevScrollY]= useState(0);
-    const [headerVisible , setHeaderVisible] = useState(true);
-  
-    const headerHeight = scrollY.interpolate({
-      inputRange: [0, screenHeight * .5 , screenHeight ],
-      outputRange: [90,  headerVisible ? 50:0, 0],
-   
-    });
- 
-    const translate = scrollY.interpolate({
-        inputRange: [0, screenHeight * 0.5 , screenHeight ],
-        outputRange: [0, 1, -1],
-        extrapolate: 'clamp',
-    })
-    const textOpacity = diffClamp.interpolate({
-        inputRange:[0 , 50, 400 ],
-        outputRange: [1, 1, 0],
-        
-        easing: Easing.inOut(Easing.ease),
-    });
-    const headerColor = scrollY.interpolate({
-        inputRange: [0, 200],
-        outputRange: ['white', 'transparent'], 
-      
-      })
-       
+    const [ headerVisible, setHeaderVisible] = useState(0);
     
+    const animatedValues = {
+         headerHeight : scrollY.interpolate({
+            inputRange: [0, screenHeight * .5 , screenHeight ],
+            outputRange: [90,  headerVisible ? 50:0, 0],
+            extrapolate: 'clamp',
+      }),
+   
+        translate : scrollY.interpolate({
+            inputRange: [0, screenHeight * 0.5 , screenHeight ],
+            outputRange: [0, 1, -1],
+            extrapolate: 'clamp',
+      }),
+        textOpacity : diffClamp.interpolate({
+            inputRange:[0 , 50, 400 ],
+            outputRange: [1, 1, 0],
+            
+            easing: Easing.inOut(Easing.ease),
+      }),
+        headerColor : scrollY.interpolate({
+            inputRange: [0, 200],
+            outputRange: ['white', 'transparent'], 
+            
+        }),
+};
+ 
+   
+    const animationDuration = 5000; 
+
       const hideHeader = () => {
-        setHeaderVisible(false);
+        Reanimated.timing(animatedValues.headerHeight,{
+            toValue: 0,
+            duration: animationDuration,
+            easing: Easing.linear,
+            useNativeDriver: false,
+        }).start(() => setHeaderVisible(false)),
+
+        Reanimated.timing(animatedValues.translate,{
+            toValue: 0,
+            duration: animationDuration,
+            easing: Easing.linear,
+            useNativeDriver: false,
+        }).start(),
+
+        Reanimated.timing(animatedValues.textOpacity,{
+            toValue: 0,
+            duration: animationDuration,
+            easing: Easing.linear,
+            useNativeDriver: false,
+        }).start(),
+
+        Reanimated.timing(animatedValues.headerColor,{
+            toValue: 0,
+            duration: animationDuration,
+            easing: Easing.linear,
+            useNativeDriver: false,
+        }).start();
+
        };
     
        const showHeader = () => {
         setHeaderVisible(true);
+        Reanimated.timing(animatedValues.headerHeight,{
+            toValue: 1,
+            duration: animationDuration,
+            easing: Easing.linear,
+            useNativeDriver: false,
+        }).start(),
+
+        
+        Reanimated.timing(animatedValues.translate,{
+            toValue: 1,
+            duration: animationDuration,
+            easing: Easing.linear,
+            useNativeDriver: false,
+        }).start(),
+
+        
+        Reanimated.timing(animatedValues.textOpacity,{
+            toValue: 1,
+            duration: animationDuration,
+            easing: Easing.linear,
+            useNativeDriver: false,
+        }).start(),
+
+        
+        Reanimated.timing(animatedValues.headerColor,{
+            toValue: 1,
+            duration: animationDuration,
+            easing: Easing.linear,
+            useNativeDriver: false,
+        }).start();
        };
     
 
-   const handleScroll = (event) => {
+    const handleScroll = (event) => {
     const offsetY = event.nativeEvent.contentOffset.y;
     const scrollVelocity = offsetY - prevScrollY;
-    if( scrollVelocity > 5 && offsetY > 5) {
+    if( scrollVelocity > 5 & offsetY > 5) {
+        
         
         hideHeader();
     } else if (scrollVelocity < 0){
+     ;
         
         showHeader();
     }
@@ -61,13 +125,15 @@ export default function Home() {
   
     return (
         <>
-        {headerVisible && (
+        {headerVisible ?(
             
        
-        <Animated.View style={[styles.header, { height: headerHeight ,  transform: [{ translateY: translate }] }]}>
-          <HeaderTitle style={styles.headerTitle } opacity={textOpacity} color={headerColor}>Coffee Snob.</HeaderTitle>
-        </Animated.View>
-        )}
+        <Reanimated.View style={[styles.header, { height: animatedValues.headerHeight ,  transform: [{ translateY: animatedValues.translate }]  }]}>
+          <HeaderTitle style={styles.headerTitle } opacity={animatedValues.textOpacity} color={animatedValues.headerColor}>
+           Coffee Snob.
+            </HeaderTitle>
+        </Reanimated.View>
+        ) : null}
         <CafeList onScroll={handleScroll} style={styles.feed} />
        </>
     );
