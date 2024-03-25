25e7d7607122a5358ea4dd229af529c8120afa89
Gracie Sharkey <graciesharkey@Gracies-MacBook-Pro.local>
2024-03-26 11:57:43 +1300

    refactor cafeData to pull cities in DropSelector component

--- coffee-snob-app/components/CafeCard.js
+++ coffee-snob-app/components/CafeCard.js
@@ -33,7 +33,8 @@ const CafeCard = ({ cafe }) => {
                />
                <Text style={styles.cardReviews}>{cafe.totalReviews}</Text>
                    </View>
-                <Text style={styles.cardLocation}>{cafe.location}</Text>
+                <Text style={styles.cardLocation}>{cafe.street},{cafe.city},</Text>
+                <Text style={styles.cardLocation}>{cafe.country}</Text>
                 <View style={styles.hoursContainer}>
                 <Text style={[styles.cardOpeningHours, { color: cafe.openingHours === 'Open' ? '#637B00' : '#AD000A'}]}>{cafe.openingHours}</Text>
                 <Text style={styles.space}></Text>
