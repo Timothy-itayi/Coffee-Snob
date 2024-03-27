557345fe23557736d87b1a84bc9943f962c54c4d
Tim Itayi <timitayi@Timothys-Mac.local>
2024-03-28 12:18:46 +1300

    add customButton component to resetFilters

--- coffee-snob-app/components/home.js
+++ coffee-snob-app/components/home.js
@@ -1,5 +1,5 @@
-import   React, {useState, useEffect}  from 'react';
-import { Animated, Text , Button} from 'react-native';
+import   React, {useState}  from 'react';
+import { Animated, Text , Button, View } from 'react-native';
 import CafeList from './cafeList';
 import HeaderTitle from './HeaderTitle';
 import styles from '../AppStyles';
@@ -7,23 +7,25 @@ import CafeFilter from './cafeFilter';
 import {cafes} from '../data/CafeData';
 import { useFilter } from './filterContext';
 import  CustomModalSelector from './CustomModalSelector';
- 
+import CustomButton from './customButton';
 const Home = () => { 
  const [selectedCity , setSelectedCity] = useState(null);
- const [prevSelectedCity, setPrevSelectedCity ] = useState(null);
  const [selectedCityMessage, setSelectedCityMessage] = useState(null)
  const { activeFilter , setActiveFilter } = useFilter(null);
 
 
  
   const handleFilterChange = (filter) => {
-    setActiveFilter(filter);
+    if (!selectedCity) {
+      setActiveFilter(filter);
+    }
+   
   };
 
   const handleOptionChange = (option) => {
       setSelectedCity(option);
       setSelectedCityMessage( `Successfully selected ${option}`);
-  
+      setActiveFilter(null)
   };
 
   const resetFilters = () => {
@@ -40,16 +42,28 @@ const Home = () => {
         >
           <Text style={styles.headerText}>Coffee Snob.</Text>
         </HeaderTitle>
-     <CustomModalSelector
-        options={cafes.map((cafe) => ({value: cafe.city, id: cafe.city}))}
-        selectedOption={selectedCity}
-        onOptionChange={handleOptionChange}
-        getSelectedItem={() => {}}
-        
-        />
-       {selectedCity && <Text style={styles.selectedCityMessage}>{selectedCityMessage}</Text>}
-       <Button title='Reset Filters' onPress={resetFilters} />
-        <CafeFilter activeFilter={activeFilter} onChangeFilter={handleFilterChange} />
+        <View style={styles.rowContainer} >
+          <CustomModalSelector
+                options={cafes.map((cafe) => ({value: cafe.city, id: cafe.city}))}
+                selectedOption={selectedCity}
+                onOptionChange={handleOptionChange}
+                getSelectedItem={() => {}}
+            />
+            <View>
+            <CustomButton 
+             style={styles.resetFilterButton} title='Reset Filters' onPress={resetFilters} />
+            </View>           
+         </View>
+         <View>
+           {selectedCity && <Text style={styles.selectedCityMessage}>{selectedCityMessage}</Text>}
+       
+         </View>
+         
+       
+    
+      
+     
+        <CafeFilter activeFilter={activeFilter} onChangeFilter={handleFilterChange} disabled={!!selectedCity} />
       </Animated.View>
 
       <CafeList
